9ef64629c6e8c2e5ae4772bb92ac7d288c8a581d
sensen <64098056@qq.com>
2021-12-03 20:15:23 +0800

    更新 Base 入口 与 Lifecycle 声明周期接口

---
+++ src/main/java/com/ss/main/BaseModularity.java
@@ -0,0 +1,13 @@
+package com.ss.main;
+
+/**
+ * @author Jangs
+ * 此类 仅提供入口方法
+ */
+public abstract class BaseModularity {
+    /**
+     * 入口方法
+     */
+    public abstract void doService();
+
+}
---
+++ src/main/java/com/ss/main/Lifecycle.java
@@ -0,0 +1,16 @@
+package com.ss.main;
+
+/**
+ * @author Jangs
+ */
+public interface Lifecycle {
+    /**
+     * 初始化
+     */
+    void init();
+
+    /**
+     * 销毁
+     */
+    void destroy();
+}
